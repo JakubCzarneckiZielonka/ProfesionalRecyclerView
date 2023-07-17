0445267a475005b7ce2dfccd17bd54abfbe8eff5
jc <jakubczarnecki@poczta.onet.pl>
2023-07-17 21:40:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationugyvbkytifcvfuyg/Movie.java
@@ -0,0 +1,12 @@
+package com.example.myapplicationugyvbkytifcvfuyg;
+
+public class Movie {
+    //tworzymy stringi
+    private String title,genre,date;
+
+    public Movie(String title, String genre, String date) {
+        this.title = title;
+        this.genre = genre;
+        this.date = date;
+    }
+}
